fc27744a59e839953f843c8e37f4962e86b4a337
madeyexz <ian.xiao@stu.pku.edu.cn>
2024-10-26 23:48:19 +0800

    push

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="./templates"), name="static")
+# app.mount("/static", StaticFiles(directory="./templates"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def home():
